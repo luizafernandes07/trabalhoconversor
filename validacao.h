b42789b1d77633ee2490b390b26f7e575a60bc00
Ana.gurita <anajuliagurita@gmail.com>
2026-05-27 22:16:00 -0300

    Cria o arquivo de cabeçalho validacao.h
    Adiciona os protótipos das funções de validação de escopo para as bases binária, octal e hexadecimal.

---
+++ validacao.h
@@ -0,0 +1,10 @@
+#ifndef VALIDACAO_H
+#define VALIDACAO_H
+
+#include <string>
+
+bool validoOctal(std::string oct);
+bool validoHex(std::string hex);
+bool validoBinario(std::string bin);
+
+#endif
